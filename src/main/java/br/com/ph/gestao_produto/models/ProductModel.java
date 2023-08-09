fd73adeb72fb18a53c6643f70ab4d380b76ea360
Pedro Henrique <p13dr0h@gmail.com>
2023-08-09 17:21:14 -0300

    feat: add hateoas on product model

--- src/main/java/br/com/ph/gestao_produto/models/ProductModel.java
+++ src/main/java/br/com/ph/gestao_produto/models/ProductModel.java
@@ -1,6 +1,7 @@
 package br.com.ph.gestao_produto.models;
 
 import jakarta.persistence.*;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -8,7 +9,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "TB_PRODUCTS")
-public class ProductModel implements Serializable {
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
     private static final long serialVersionUID = 1l;
 
     @Id
@@ -42,4 +43,6 @@ public class ProductModel implements Serializable {
     public void setValue(BigDecimal value) {
         this.value = value;
     }
+
+
 }
