b095891742fb8ae0cb9300d89f03361e0748e63e
Pedro Henrique <p13dr0h@gmail.com>
2023-08-08 22:41:24 -0300

    chore: setup project

---
+++ src/main/java/br/com/ph/gestao_produto/GestaoProdutoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ph.gestao_produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoProdutoApplication.class, args);
+	}
+
+}
